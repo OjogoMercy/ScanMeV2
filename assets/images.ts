db61ced08e3aeacc12e85566b72d50ae6bf6872e
Ojogo Mercy <mercyojogo@gmail.com>
2025-11-09 01:43:15 +0100

    Add LoginScreen and Splash components with basic structure; include image assets

---
+++ assets/images.ts
@@ -0,0 +1,6 @@
+export const images = {
+    icon: require('./images/scanIcon.png'),
+    splash: require('./images/scanLogo.png'),
+    adaptive_icon: require('./adaptive-icon.png'),
+    favicon: require('./favicon.png'),
+};
