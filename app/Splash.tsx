ae0bfb7632a935f26ba55a8b819ec4e5d69912fb
Ojogo Mercy <mercyojogo@gmail.com>
2025-11-09 01:55:32 +0100

    Implement Splash screen with image and navigation to LoginScreen; set initial route in layout

--- app/Splash.tsx
+++ app/Splash.tsx
@@ -1,10 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { images } from '@/assets/images'
+import { useEffect } from 'react'
+import { useNavigation } from 'expo-router'
+import { useRouter } from 'expo-router'
 
 const Splash = () => {
+const router = useRouter()
+    useEffect(() => {
+        const timer = setTimeout(() => {
+
+router.replace('./LoginScreen')}, 3000)
+    },[router])
   return (
-    <View>
-      <Text>Splash</Text>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Image source={images.splash} style={{ height:'25%', width:'50%'}} />
     </View>
   )
 }
