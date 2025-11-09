cd5511ca1ede09bf264fa17aa96496ede65758de
Ojogo Mercy <mercyojogo@gmail.com>
2025-11-09 01:58:51 +0100

    Refactor Splash component to improve styling and remove unused navigation code; clean up images asset file by removing unused icons

--- app/Splash.tsx
+++ app/Splash.tsx
@@ -2,18 +2,18 @@ import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { images } from '@/assets/images'
 import { useEffect } from 'react'
-import { useNavigation } from 'expo-router'
 import { useRouter } from 'expo-router'
+import { Colors } from '@/constants/Theme'
 
 const Splash = () => {
 const router = useRouter()
-    useEffect(() => {
-        const timer = setTimeout(() => {
+//     useEffect(() => {
+//         const timer = setTimeout(() => {
 
-router.replace('./LoginScreen')}, 3000)
-    },[router])
+// router.replace('./LoginScreen')}, 3000)
+//     },[router])
   return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor:Colors.primary }}>
           <Image source={images.splash} style={{ height:'25%', width:'50%'}} />
     </View>
   )
--- assets/images.ts
+++ assets/images.ts
@@ -1,6 +1,4 @@
 export const images = {
     icon: require('./images/scanIcon.png'),
     splash: require('./images/scanLogo.png'),
-    adaptive_icon: require('./adaptive-icon.png'),
-    favicon: require('./favicon.png'),
 };
