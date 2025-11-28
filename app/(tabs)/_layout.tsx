048ec77449bbfb2ce413b99332b10ec81b71e1a8
Ojogo Mercy <mercyojogo@gmail.com>
2025-11-28 14:16:39 +0100

    ..

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,35 +2,53 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 import { Ionicons } from '@expo/vector-icons';
 import { Colors } from '../../constants/theme';
+import {CameraIcon as CameraOutline, HomeIcon as HomeOutline} from 'react-native-heroicons/outline'
+import {CameraIcon,HomeIcon} from 'react-native-heroicons/solid'
 
 
 
 export default function TabLayout() {
-  const tintColor = Colors.primary; // Use a color variable
+  const tintColor = Colors.primary; 
 
   return (
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: tintColor,
         headerShown: false,
+        animation: 'fade',
+        transitionSpec: {
+          animation: 'timing',
+          config: {
+            duration: 500
+          }
+        }
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? 'home' : 'home-outline'} color={color} size={24} />
-          )
+         tabBarIcon: ({ color, focused }) => (
+            focused ? (
+              <HomeIcon color={color} size={30} />
+            ) : (
+              <HomeOutline color={color} size={24} />
+            )
+          ),
         }}
       />
       <Tabs.Screen
-        name="CammeraScreen" 
+        name="CameraScreen" 
         options={{
           title: 'Scan',
           tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? 'home' : 'home-outline'} color={color} size={24} />
-          ),
+            
+              focused ? (
+              <CameraIcon color={color} size={30} />
+              ):(
+              <CameraOutline color={color} size={24} />
+            )
+           ),
         }}
       />
 
@@ -39,7 +57,7 @@ export default function TabLayout() {
         options={{
           title: 'History',
           tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? 'time' : 'time-outline'} color={color} size={24} />
+            <Ionicons name={focused ? 'time' : 'time-outline'} color={color} size={focused ? 30 :24} />
           ),
         }}
       />
