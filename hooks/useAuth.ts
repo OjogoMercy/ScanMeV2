d6c777b98d69744abecb21e21b5283c89ca26627
Ojogo Mercy <mercyojogo@gmail.com>
2025-12-17 17:11:21 +0100

    ..

---
+++ hooks/useAuth.ts
@@ -0,0 +1,24 @@
+// src/hooks/useAuth.ts
+import { useEffect, useState } from "react";
+import { auth } from "../";
+import { signInAnonymously, onAuthStateChanged } from "firebase/auth";
+
+export function useAuth() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, async (u) => {
+      if (!u) {
+        await signInAnonymously(auth);
+      } else {
+        setUser(u);
+        setLoading(false);
+      }
+    });
+
+    return unsub;
+  }, []);
+
+  return { user, loading };
+}
